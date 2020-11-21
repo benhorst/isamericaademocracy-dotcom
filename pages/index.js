11b149d7c93a45d3c6419f3ff3b89c3780f94ef2
Ben Horst <benpub@benhorst.com>
2020-11-21 00:14:09 -0800

    add bbc citation and image

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -15,11 +16,21 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-         Yes. As of 11/20/2020.
+         Yes. As of 11/20/2020, America is still a democracy.
         </p>
+        <div>
+          <Image
+        src="/national_votes-nc.png"
+        alt="the 2020 popular vote tally for biden winning 2020"
+        width="500"
+        height="500"
+      />
+        </div>
       </main>
       <footer className={styles.footer}>
-        
+        If you want to know what's really going on, look at what outsiders say. Here's the BBC. The BBC has way less vested interest than anything domestic, yeah? You don't trust the local media? OK. Try something overseas that has no reason to lie to you.
+        <a href="https://www.bbc.com/news/election-us-2020-54783016">[BBC]</a>
+        <br />
       </footer>
     </div>
   )
